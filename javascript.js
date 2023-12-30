4b1a77a02e55589d7693942a71170305b3a16506
Brandon Dixon <bdxn86@gmail.com>
2023-12-29 16:57:44 -0800

    Functions, Declaring variables, console.log and arrow function

---
+++ javascript.js
@@ -0,0 +1,17 @@
+// Functions
+function add(a, b)
+{
+    return a + b;
+}
+
+// Declaring variables
+// let -
+// var - 
+// const -
+x = 9;
+console.log(x);
+
+// => arrow function - shorter syntax for writing functions
+
+
+// console.log(add(2, 3));
